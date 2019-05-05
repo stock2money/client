4b242b85bed5e6f755c62bb96859abf3ed36536c
x705896204x <705896204@qq.com>
2019-05-05 19:02:50 +0800

    Update info.js

--- pages/info/info.js
+++ pages/info/info.js
@@ -1,66 +1,224 @@
-// pages/info/info.js
+// pages/inf/inf.js
+
 Page({
+  
+ 
 
   /**
+
    * 页面的初始数据
+
    */
+
   data: {
+    
+    'headLineList': [
+      
+      {
+        
+        'title': '**' ,
+        
+        'source': '**',
+        
+        'date': '2019-05-01'
 
+      }, {
+        
+        'title': '**',
+        
+        'source': '**',
+        
+        'date': '2019-05-01'
+
+      }, {
+        
+        'title': '**',
+        
+        'source': '**',
+        
+        'date': '2019-05-01'
+
+      },
+      
+    ],
+    
   },
+  
+ 
 
   /**
+
    * 生命周期函数--监听页面加载
+
    */
+
   onLoad: function (options) {
+    
 
+    
   },
+  
+  // 资讯
 
+  jumpDetails: function (e) {
+    
+    console.log(e.currentTarget.dataset.title)
+    
+    wx.navigateTo({
+      url: '../news/news?title=' + e.currentTarget.dataset.title,
+      
+      
+      success: function(res) {
+        
+        // success
+        
+      },
+      
+      fail: function () {
+        
+        // fail
+        
+      },
+      
+      complete: function() {
+        
+        // complete
+        
+      }
+
+    })
+    
+  },
+  
   /**
+
    * 生命周期函数--监听页面初次渲染完成
+
    */
+
   onReady: function () {
+    
 
+    
   },
+  
+ 
 
   /**
+
    * 生命周期函数--监听页面显示
+
    */
+
   onShow: function () {
+    
 
+    
   },
+  
+ 
 
   /**
+
    * 生命周期函数--监听页面隐藏
+
    */
+
   onHide: function () {
+    
 
+    
   },
+  
+ 
 
   /**
+
    * 生命周期函数--监听页面卸载
+
    */
+
   onUnload: function () {
+    
 
+    
   },
+  
+ 
 
   /**
+
    * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
 
+   */
+
+  onPullDownRefresh: function () {
+    
+
+    
   },
+  
+ 
 
   /**
+
    * 页面上拉触底事件的处理函数
+
    */
+
   onReachBottom: function () {
 
+    
   },
+  
+ 
 
   /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
+   * 用户点击右上角分享
+
+   */
+
+  onShareAppMessage: function () {
+    
+    return {
+      
+      title: '微信小程序联盟',
+      
+      desc: '最具人气的小程序开发联盟!',
+      
+      path: '/page/user?id=123'
+
+    }
+    
+  },
+  
+ 
+
+  /**
+
+   * 页面上拉触底事件的处理函数
+
+   */
+
+  onReachBottom: function () {
+    
+
+    
+  },
+  
+ 
+
+  /**
+
+   * 页面相关事件处理函数--监听用户下拉动作
+
+   */
+
+  onPullDownRefresh: function () {
+    
+
+    
   }
+
 })
